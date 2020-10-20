c806cddf297cb55c52f285c7d382c771bd18dd51
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-10-20 22:30:12 +0800

    nethunt wrapper test

--- tests/nethunt.test.js
+++ tests/nethunt.test.js
@@ -1,65 +1,47 @@
-require('dotenv').config()
 const { expect } = require('chai')
 const Nethunt = require('../index')
 
-describe('nethunt', () => {
-  const nethunt = new Nethunt(
-    process.env.NETHUNT_USERNAME,
-    process.env.NETHUNT_PASSWORD
-  )
-
-  it('accepts username', () => {
-    expect(nethunt.username).to.equal(process.env.NETHUNT_USERNAME)
+describe('wrapper', () => {
+  context('no username given', () => {
+    it('throws error: Username cannot be blank', () => {
+      expect(() => new Nethunt(false, 'password')).to.throw('Username cannot be blank')
+    })
   })
-
-  it('accepts password', () => {
-    expect(nethunt.password).to.equal(process.env.NETHUNT_PASSWORD)
+  context('no password given', () => {
+    it('throws error: Password cannot be blank', () => {
+      expect(() => new Nethunt('username', false)).to.throw('Password cannot be blank')
+    })
   })
+  context('username and password given', () => {
+    it('creates the wrapper', () => {
+      expect(() => new Nethunt('username', 'password')).to.not.throw
+    })
 
-  it('errors when no username', () => {
-    expect(() => new Nethunt(false, 'password')).to.throw('Username cannot be blank')
+    it('has option: headers', () => {
+      const client = new Nethunt('username', 'password')
+      expect(client.options).to.include.all.keys('headers')
+    })
+
+    it('has headers: Authorization', () => {
+      const client = new Nethunt('username', 'password')
+      expect(client.options.headers).to.include.all.keys('Authorization')
+    })
+
+    it('has headers: Content-Type', () => {
+      const client = new Nethunt('username', 'password')
+      expect(client.options.headers).to.include.all.keys('Content-Type')
+    })
+
+    it('authorization: is basic', () => {
+      const client = new Nethunt('username', 'password')
+      expect(client.options.headers.Authorization).to.have.string('Basic')
+    })
+
+    it('Content-Type: is application/json', () => {
+      const client = new Nethunt('username', 'password')
+      expect(client.options.headers['Content-Type']).to.equal('application/json')
+    })
+
+
   })
-
-  it('errors when no password', () => {
-    expect(() => new Nethunt('username', false)).to.throw('Password cannot be blank')
-  })
-
-  it('readableFolder', async () => {
-    const res = await nethunt.readableFolder()
-    expect(res).to.be.an('array')
-  })
-
-  it('writableFolder', async () => {
-    const res = await nethunt.writableFolder()
-    expect(res).to.be.an('array')
-  })
-
-  it('folderField', async () => {
-    const res = await nethunt.folderField(
-      process.env.NETHUNT_FOLDERID
-    )
-    expect(res).to.be.an('array')
-  })
-
-  it('findRecord', async () => {
-    const res = await nethunt.findRecord(
-      process.env.NETHUNT_FOLDERID,
-      { created: "today" }
-    )
-    expect(res).to.be.an('array')
-  })
-
-  it('updateRecord', async () => {
-    const res = await nethunt.updateRecord(
-      process.env.NETHUNT_RECORDID, {
-      fieldActions: {
-        'Téléphone': {
-          add: ['0682666699'],
-          overwrite: true
-        },
-      }
-    }
-    )
-  })
-
 })
